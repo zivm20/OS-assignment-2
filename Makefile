5ed6d59010be0ebe57f59ba4bddf3f907b704e8b
Ziv Morgan <67800437+zivm20@users.noreply.github.com>
2022-11-28 16:41:30 +0200

    done first part

--- Makefile
+++ Makefile
@@ -17,5 +17,8 @@ mync: mync.c
 %.so: %.o
 	$(CXX) -shared -o $@ $<
 
+
+#valgrind --tool=memcheck -v --leak-check=full --show-leak-kinds=all  --error-exitcode=99 ./mync 2>&1 | { egrep "lost| at " || true; }
+#valgrind --tool=memcheck -v --leak-check=full --show-leak-kinds=all  --error-exitcode=99 ./mync 127.0.0.1 299934 2>&1 | { egrep "lost| at " || true; }
 clean:
 	rm -f *.o mytee mync
--- mync.c
+++ mync.c
@@ -8,8 +8,7 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 
-#define MESSAGE_SIZE 1024
-#define MESSAGE_COUNT 5
+
 
 int send_data(const char* content, const int sock, const size_t len){
     
@@ -46,14 +45,8 @@ int main(int argc, char **argv) {
         int port = atoi(argv[2]);
         char* ip = argv[1];
         
-        //open TCP socket
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (sock == -1) {
-            perror("socket\n");
-            return -1;
-        } 
 
-        //connect to server
+        //server socket
         struct sockaddr_in server;
         memset(&server,0,sizeof(server));
         server.sin_port = htons(port);
@@ -62,26 +55,31 @@ int main(int argc, char **argv) {
             return -1;
         }
         server.sin_family = AF_INET;
-        int connection = connect(sock,(struct sockaddr*)&server,sizeof(server));
-        if(connection == -1){
-            perror("connection failed!\n");
-        }
+        
         char *line = NULL;
         size_t len = 0;
         ssize_t read;
         while ((read = getline(&line, &len, stdin)) != -1) {
-            printf("%s",line);
+            //open TCP socket
+            int sock = socket(AF_INET, SOCK_STREAM, 0);
+            if (sock == -1) {
+                perror("socket\n");
+                return -1;
+            } 
+            int connection = connect(sock,(struct sockaddr*)&server,sizeof(server));
+            if(connection == -1){
+                perror("connection failed!\n");
+            }
+        
+            
             if(send_data(line,sock,read)<0){
                 free(line);
                 close(sock);
                 return -1;
             }
-            
-            
+            close(sock);
         }
-        printf("done");
         free(line);
-        close(sock); 
     }
     else{
         
@@ -93,13 +91,7 @@ int main(int argc, char **argv) {
             perror("socket\n");
             return -1;
         } 
-        /*
-        int yes = 1;
-        if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1){ 
-            perror("setsockopt"); 
-            return -1;
-        }
-        */
+        
 
         //bind socket
         struct sockaddr_in server;
@@ -122,25 +114,24 @@ int main(int argc, char **argv) {
             return -1;
         }
         
-        //accept client connection
-        struct sockaddr_in clientAddress;
-        socklen_t clientAddressLen = sizeof(clientAddress);
-        memset(&clientAddress, 0, sizeof(clientAddress));
-        clientAddressLen = sizeof(clientAddress);
-        int clientSocket = accept(sock, (struct sockaddr *)&clientAddress, &clientAddressLen);
+        char c;
+        //first packet is a packet containing the size of the data
+        while(1){
+            //accept client connection
+            struct sockaddr_in clientAddress;
+            socklen_t clientAddressLen = sizeof(clientAddress);
+            memset(&clientAddress, 0, sizeof(clientAddress));
+            clientAddressLen = sizeof(clientAddress);
+            int clientSocket = accept(sock, (struct sockaddr *)&clientAddress, &clientAddressLen);
+
         if (clientSocket == -1){
             perror("Failed to accept client\n");
             return -1;
         }
-
-        
-        char c;
-        //first packet is a packet containing the size of the data
-        while(1){
-                   
             while(recv(clientSocket,&c,sizeof(char),0) > 0){
                 printf("%c",c);
             }
+
             
         }
         
@@ -148,6 +139,6 @@ int main(int argc, char **argv) {
          
     }
 
-    return 1;
+    return 0;
 }
     
